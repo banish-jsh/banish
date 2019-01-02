5a7acf7ee28c6cced7416b05baba0c515ff65677
yu.jiajun <yujiajun_50050@wdai.com>
2019-01-02 10:17:16 +0800

    初始化代码

---
+++ banish-webapp/src/main/java/com/yy/design/banishwebapp/BanishWebappApplication.java
@@ -0,0 +1,14 @@
+package com.yy.design.banishwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanishWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanishWebappApplication.class, args);
+    }
+
+}
+
